b96a1eb95de364c079d0dee143aefb55b8b3b761
Nikolay Dolgopolov <dolgopolov.n.v@gmail.com>
2017-05-11 13:07:19 +0700

    Initial Commit

---
+++ testAddress/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testAddress
+//
+//  Created by ndolgopolov on 11.05.17.
+//  Copyright © 2017 ndolgopolov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testAddress/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  testAddress
+//
+//  Created by ndolgopolov on 11.05.17.
+//  Copyright © 2017 ndolgopolov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
